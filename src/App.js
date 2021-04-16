1fb6c109c94ef836ea60ee246d98f517d48b475b
Cesar Alvarez Vallero <csralvarezvallero12@gmail.com>
2021-04-16 11:03:21 -0300

    navbar component modularized

--- src/App.js
+++ src/App.js
@@ -1,29 +1,11 @@
 import './App.css';
 
+import { NavBar } from './components/navbar/navbar';
+
 function App() {
   return (
     <div className="App">
-      <header className="nav-bar">
-        <p>
-          Market Dex
-        </p>
-        <nav className="nav-menu">
-          <ul className="nav-menu-list">
-            <li className="nav-menu-item">
-              <a href=''>Home</a>
-            </li>
-            <li className="nav-menu-item">
-              <a href=''>Articulos</a>
-            </li>
-            <li className="nav-menu-item">
-              <a href=''>Nosotros</a>
-            </li>
-            <li className="nav-menu-item">
-              <a href=''>Contacto</a>
-            </li>
-          </ul>
-        </nav>
-      </header>
+      <NavBar />
     </div>
   );
 }
