e3a4ab50da03ab090a66cc6677f05a23fb7ad00a
Cesar Alvarez Vallero <csralvarezvallero12@gmail.com>
2021-04-17 16:25:39 -0300

    first draft of search box

---
+++ src/components/navsearch/navsearch.js
@@ -0,0 +1,20 @@
+import './navsearch.css'
+import magnifyingGlass from '../../img/png/magnifying-glass-search.png'
+
+export const NavSearch = () => {
+  return (
+    <form className="nav-search">
+      <input
+        type="text"
+        className="nav-search-input"
+        placeholder="Busca todo lo que necesitas..."
+        maxLength="80"
+      ></input>
+      <button type="submit" className="nav-search-button">
+        <img
+					src={magnifyingGlass}
+				/>
+      </button>
+    </form>
+  );
+}
