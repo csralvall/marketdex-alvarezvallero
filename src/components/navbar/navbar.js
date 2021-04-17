8b6dae0c2687df9552a7e5852df5cf2691dcc1aa
Cesar Alvarez Vallero <csralvarezvallero12@gmail.com>
2021-04-17 16:21:10 -0300

    added personalized logo to navbar

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,13 +1,18 @@
 import './navbar.css';
+import ecommerceLogo from '../../img/png/ecommerce_logo.png'
 
 import { NavMenu } from '../navmenu/navmenu'
+import { NavSearch } from '../navsearch/navsearch'
 
 export const NavBar = () => {
   return (
     <header className="nav-bar">
-      <p>
-        Market Dex
-      </p>
+      <img
+        src={ecommerceLogo}
+        width="180px"
+        height="50px" 
+      />
+      <NavSearch />
       <NavMenu />
     </header>
   );
