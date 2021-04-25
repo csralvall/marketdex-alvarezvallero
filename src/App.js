370b9fdc1f891cc3c91afa684b69a2dc5694daa4
Cesar Alvarez Vallero <csralvarezvallero12@gmail.com>
2021-04-25 00:26:07 -0300

    added ItemListContainer component inside App - further improvement needed

--- src/App.js
+++ src/App.js
@@ -1,41 +1,13 @@
 import './App.css';
 
 import { NavBar } from './components/NavBar/NavBar';
-import { ItemList } from './components/ItemListContainer/ItemListContainer';
+import { ItemListContainer } from './components/ItemListContainer/ItemListContainer';
 
 function App() {
-  const products = [
-    {
-      title: "Raqueta",
-      price: "50",
-      description: "No es la de Federer",
-    },
-    {
-      title: "Pelota",
-      price: "5",
-      description: "Esta pinchada",
-    },
-    {
-      title: "Vincha",
-      price: "345",
-      description: "Pero si sos pelado",
-    },
-    {
-      title: "Remera",
-      price: "45",
-      description: "Se la olvido un cliente",
-    },
-    {
-      title: "Shorts",
-      price: "55",
-      description: "Los hizo la vecina a crochet",
-    },
-  ];
-
   return (
     <div className="App">
       <NavBar />
-      <ItemList products={products} />
+      <ItemListContainer />
     </div>
   );
 }
