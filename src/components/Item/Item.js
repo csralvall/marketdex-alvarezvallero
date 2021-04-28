a0b841c52f4fc13fd8e8147e63419ef522d392f8
Cesar Alvarez Vallero <csralvarezvallero12@gmail.com>
2021-04-28 18:19:15 -0300

    styles added to product cards

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -5,7 +5,7 @@ export const Item = ( { title, price, description, image } ) => {
   return (
     <li className='product-item'>
       <h2 className='product-name'>{title}</h2>
-      <p className='product-price'>{price}</p>
+      <p className='product-price'>$ {price}</p>
       <img className='product-image' src={image}
         width="180px"
         height="180px" 
