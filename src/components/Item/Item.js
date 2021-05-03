390bf4b5b9433ecf97bd61f2b9a23c942a836478
Cesar Alvarez Vallero <csralvarezvallero12@gmail.com>
2021-05-02 22:03:52 -0300

    added redirection to item details after item click in Item components

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -1,14 +1,22 @@
 import './Item.css'
 
-export const Item = ( { title, price, image } ) => {
+import { NavLink } from 'react-router-dom'
+
+export const Item = ( { itemId, title, price, image } ) => {
   return (
-    <li className='product-item'>
-      <h2 className='product-name'>{title}</h2>
-      <p className='product-price'>$ {price}</p>
-      <img className='product-image' src={image}
-        width="180px"
-        height="180px" 
-      />
-    </li>
+    <NavLink
+      exact to={`/item/${itemId}`}
+      className='detail-link'
+      activeClassName='active-detail-link'
+    >
+      <li className='product-item'>
+        <h2 className='product-name'>{title}</h2>
+        <p className='product-price'>$ {price}</p>
+        <img className='product-image' src={image}
+          width="180px"
+          height="180px" 
+        />
+      </li>
+    </ NavLink>
   );
 }
