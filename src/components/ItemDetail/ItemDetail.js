0ea5dc441ff7585ad89da15b4956eae6f185259d
Cesar Alvarez Vallero <csralvarezvallero12@gmail.com>
2021-05-07 10:40:57 -0300

    added button to finish buy on quantity submit

--- src/components/ItemDetail/ItemDetail.js
+++ src/components/ItemDetail/ItemDetail.js
@@ -1,8 +1,36 @@
 import './ItemDetail.css'
 
+import { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
+
 import { ItemCount } from '../ItemCount/ItemCount'
 
 export const ItemDetail = ({ title, description, image, price }) => {
+  const [ quantity, setQuantity ] = useState(0);
+  const onAdd = (quantityToAdd) => {
+    setQuantity(quantityToAdd);
+  };
+  const itemCount = <ItemCount stock={5} initial={0} onAdd={onAdd} />;
+  const finishBuy = (
+    <Link to='/cart'>
+      <button className='finish-buy'>Finish buy</button>
+    </Link>
+  );
+  const [ cartButton, setCartButton ] = useState(itemCount);
+
+  useEffect(() => {
+    let mounted = true;
+
+    const setButton = () => {
+      if(quantity) setCartButton(finishBuy);
+    }
+
+    if(mounted) setButton();
+
+    return () => mounted = false;
+
+  }, [quantity])
+
   return (
     <div className='product-detail'>
       <img className='product-detail-image' src={image} />
@@ -10,10 +38,8 @@ export const ItemDetail = ({ title, description, image, price }) => {
         <h2 className='product-detail-name'>{title}</h2>
         <p className='product-detail-price'>$ {price}</p>
         <p className='product-detail-description'>{description}</p>
-      <div className='product-detail-count'>
-        <ItemCount stock={5} initial={0} />
+        <div className='product-detail-cart'>{cartButton}</div>
       </div>
-    </div>
     </div>
   );
 }
