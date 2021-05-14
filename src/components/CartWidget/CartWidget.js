c10088027429b86ded07a91aeaf8079bf3e421be
Cesar Alvarez Vallero <csralvarezvallero12@gmail.com>
2021-05-14 19:53:58 -0300

    added quantity indicator and hidden on empty property to cartwidget

--- src/components/CartWidget/CartWidget.js
+++ src/components/CartWidget/CartWidget.js
@@ -1,18 +1,32 @@
 import './CartWidget.css';
 
+import { useContext } from 'react';
+import { NavLink } from 'react-router-dom';
+
 import marketBag from '../../assets/svg/marketBag.svg';
+import { CartContext } from '../../context/CartContext';
 
 export const CartWidget = () => {
-  const cartWidgetAlert = () => alert('Aca van tus compras!!');
+  const { quantity } = useContext(CartContext);
 
   return (
-    <button onClick={cartWidgetAlert} type='button' className='cart-widget-button'>
+    <NavLink exact to='/cart'
+      className={`cart-widget-button ${quantity ? 'with-items' : ''}`} >
       <img
         src={marketBag}
         alt='the cart where your buys are stored'
         width='45em'
         height='45em'
       />
-    </button>
+      <svg width='50px' height='50px'>
+        <g>
+          <circle cx='38' cy='40' r='9'></circle>
+          <text fill='white' x='38' y='40' font-size='10'
+            text-anchor='middle' alignment-baseline='central'>
+            {(quantity < 100) ? quantity : '...'}
+          </text>
+        </g>
+      </svg>
+    </NavLink>
   );
 }
