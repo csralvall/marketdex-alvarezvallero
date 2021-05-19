6001b3eba2b32b0000fbb5c4231e19f231c4e00f
Cesar Alvarez Vallero <csralvarezvallero12@gmail.com>
2021-05-19 12:11:44 -0300

    imports corrected to avoid double app initialization

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,14 +1,18 @@
-import firebase from 'firebase';
+import firebase from 'firebase/app';
 
-const app = firebase.initializeApp({
+import 'firebase/firestore';
+
+const config = {
   apiKey: "AIzaSyA3y-WRu-2olfBJVfW8sUUP85YJo9c20GQ",
   authDomain: "marketdex-ecommerce.firebaseapp.com",
   projectId: "marketdex-ecommerce",
   storageBucket: "marketdex-ecommerce.appspot.com",
   messagingSenderId: "590253481582",
   appId: "1:590253481582:web:388c085172bea9f432107f"
-});
+};
 
-export const getFirebase = () => app;
+const ap = !firebase.apps.length ? firebase.initializeApp(config) : firebase.app()
 
-export const getFirestore = () => firebase.firestore(app);
+export const getFirebase = () => ap;
+
+export const getFirestore = () => firebase.firestore(ap);
