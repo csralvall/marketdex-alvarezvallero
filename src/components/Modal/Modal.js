b8048f1d5cf1004185c1d79cc0ceabfe1c1346b7
Cesar Alvarez Vallero <csralvarezvallero12@gmail.com>
2021-05-23 16:47:24 -0300

    added modal component with custom hook

---
+++ src/components/Modal/Modal.js
@@ -0,0 +1,30 @@
+import './Modal.css';
+
+import { createPortal } from 'react-dom';
+
+export const Modal = ({ children, isShowing, hide }) => (
+  isShowing ? createPortal(
+    <div className='modal-overlay'>
+      <div
+					className='modal-wrapper'
+					aria-modal
+					aria-hidden
+					tabIndex={-1}
+					role="dialog"
+			>
+        <div className='modal'>
+          {children}
+          <button
+            type='button'
+            className='modal-close-button'
+            data-dismiss='modal'
+            aria-label='close'
+            onClick={hide}
+          >
+            Close
+          </button>
+        </div>
+      </div>
+    </div>, document.body
+  ) : null
+);
