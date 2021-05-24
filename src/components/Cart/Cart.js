72d44db60c74c458554bff2455f77331e62f5bdd
Cesar Alvarez Vallero <csralvarezvallero12@gmail.com>
2021-05-24 15:25:28 -0300

    Add purchase order submission
    Until this commit, the user of this application can only add and remove
    items from his cart, but he cannot make and submit his purchase order to
    the database.
    
    With this commit, the user can add, remove and submit his purchase.

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,18 +1,54 @@
 import './Cart.css';
 
-import { useContext, Fragment } from 'react';
+import { useState, useContext, Fragment } from 'react';
 import { NavLink } from 'react-router-dom';
+import firebase from 'firebase/app';
+
+import { useModal } from '../../hooks/useModal';
 
 import { CartContext } from '../../context/CartContext';
 import { CartItem } from '../CartItem/CartItem';
+import { UserForm } from '../UserForm/UserForm';
+import { getFirestore } from '../../firebase/firebase';
+import { Modal } from '../Modal/Modal';
 
 export const Cart = () => {
   const { cart, removeFromCart, clear, quantity, cost } = useContext(CartContext);
+  const {isShowing, toggle} = useModal();
+  const [orderId, setOrderId] = useState('');
 
   const removeItem = (itemId) => removeFromCart(itemId);
 
+  const submitOrder = async (e, buyerData, clearForm) => {
+    e.preventDefault();
+    const db = getFirestore();
+    const orders = db.collection('orders');
+    const newOrder = {
+      buyer: buyerData,
+      items: cart,
+      date: firebase.firestore.FieldValue.serverTimestamp(),
+      total: cost,
+    };
+
+    try {
+      const returnedId = await orders.add(newOrder);
+      const batch = db.batch();
+      cart.forEach((item) => {
+        const itemSelected = db.collection('products').doc(item.id);
+        batch.update(itemSelected, { stock: item.stock - item.quantity })
+      })
+      await batch.commit()
+      clear();
+      clearForm();
+      setOrderId(returnedId.id);
+      toggle();
+    } catch (error) {
+      console.error(error);
+    };
+  };
+
   return (
-    <Fragment>
+    <div className='cart-view'>
       <div className='cart-resume'>
         { quantity ? 
           <Fragment>
@@ -44,6 +80,11 @@ export const Cart = () => {
             />
           ))}
       </ul>
-    </Fragment>
+      { quantity ? <UserForm onSubmit={submitOrder} /> : null }
+      <Modal isShowing={isShowing} hide={toggle}>
+        <h2>Order submitted successfully</h2>
+        <p>Order ID: <i>{orderId}</i></p>
+      </Modal>
+    </div>
   );
 }
