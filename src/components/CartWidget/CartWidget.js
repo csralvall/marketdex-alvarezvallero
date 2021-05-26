31b9ca1bdc4dcea67d1d90d432abdddca7c4815f
Cesar Alvarez Vallero <csralvarezvallero12@gmail.com>
2021-05-25 22:06:34 -0300

    Change cart display logic
    Until previous versions, this component only was displayed when the cart
    had items on it, and a blue circle in the left bottom corner with a
    number in its center showed the total quantity of them.
    
    Now, the cart icon will be displayed all the time and will redirect to
    the cart view. The total quantity of items into the cart is going to be
    displayed only when it has at least one item inside, using the same blue
    circle just mentioned before.

--- src/components/CartWidget/CartWidget.js
+++ src/components/CartWidget/CartWidget.js
@@ -11,22 +11,24 @@ export const CartWidget = () => {
 
   return (
     <NavLink exact to='/cart'
-      className={`cart-widget-button ${quantity ? 'with-items' : ''}`} >
+      className='cart-widget-button'>
       <img
         src={marketBag}
         alt='the cart where your buys are stored'
         width='45em'
         height='45em'
       />
-      <svg width='50px' height='50px'>
-        <g>
-          <circle cx='38' cy='40' r='9'></circle>
-          <text x='38' y='40' fontSize='13'
-            textAnchor='middle' alignmentBaseline='central'>
-            {(quantity < 100) ? quantity : '...'}
-          </text>
-        </g>
-      </svg>
+      <div className={`${quantity ? 'with-items' : 'without-items'}`}>
+        <svg width='50px' height='50px'>
+          <g>
+            <circle cx='38' cy='40' r='9'></circle>
+            <text x='38' y='40' fontSize='13'
+              textAnchor='middle' alignmentBaseline='central'>
+              {(quantity < 100) ? quantity : '...'}
+            </text>
+          </g>
+        </svg>
+      </div>
     </NavLink>
   );
 }
