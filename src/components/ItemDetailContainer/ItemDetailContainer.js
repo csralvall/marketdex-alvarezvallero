1ac59ff0a3ec1e548bbe167c0cf31de98664f037
Cesar Alvarez Vallero <csralvarezvallero12@gmail.com>
2021-05-28 21:22:31 -0300

    Add dynamic behavior to component views
    The behavior of the container components on failure or slow loading was
    not defined. With this modification to code, the app will display a
    loading animation until all items be fetched from the database. Also, if
    the query to database results empty (items not found), another view will
    be displayed, explaining the absence of the expected items.

--- src/components/ItemDetailContainer/ItemDetailContainer.js
+++ src/components/ItemDetailContainer/ItemDetailContainer.js
@@ -5,6 +5,7 @@ import { useParams } from 'react-router';
 
 import { ItemDetail } from '../ItemDetail/ItemDetail';
 import { ResourceNotFound } from '../ResourceNotFound/ResourceNotFound';
+import { LoaderAnimation } from '../LoaderAnimation/LoaderAnimation';
 import { CartContext } from '../../context/CartContext';
 import { getFirestore } from '../../firebase/firebase';
 
@@ -12,42 +13,50 @@ export const ItemDetailContainer = () => {
   const { itemId } = useParams();
   const { addToCart } = useContext(CartContext);
   const [item, setItem] = useState([]);
-  const [isItemAvailable, setIsItemAvailable] = useState(false);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
+    setLoading(true);
     const db = getFirestore();
     const itemCollection = db.collection('products');
 
     const documentReference = itemCollection.doc(itemId);
     const unsuscribe = documentReference.onSnapshot((querySnapshot) => {
-      if (querySnapshot.exists) {
-        setIsItemAvailable(true);
-        setItem(querySnapshot.data());
-      } else {
-        setIsItemAvailable(false);
-      }
+      setItem(querySnapshot);
+      setLoading(false);
     }, (error) => console.error(error));
 
     return () => unsuscribe();
 
   }, [itemId]);
 
+  const selectView = (item) => {
+    let view = (
+      <ResourceNotFound> 
+        <p className='not-found-message'>We are sorry, item not found.</p>
+      </ResourceNotFound> 
+    );
+
+    if(item.exists) {
+      const product = item.data();
+      view = (
+        <ItemDetail
+          title={product.title}
+          description={product.description}
+          image={product.image}
+          price={product.price}
+          stock={product.stock}
+          onBuy={(count) => addToCart({...product, quantity: count})}
+        />
+      );
+    }
+
+    return view;
+  };
+
   return (
     <Fragment>
-    { isItemAvailable ? 
-      <ItemDetail
-        title={item.title}
-        description={item.description}
-        image={item.image}
-        price={item.price}
-        stock={item.stock}
-        onBuy={(count) => addToCart({...item, quantity: count})}
-      />
-      :
-        <ResourceNotFound> 
-          <p className='not-found-message'>We are sorry, item not found.</p>
-        </ResourceNotFound> 
-      }
+      { loading ? <LoaderAnimation /> : selectView(item) }
     </Fragment>
   );
 }
--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -1,13 +1,31 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, Fragment } from 'react';
 import { PropTypes } from 'prop-types';
 
 import { ItemList } from '../ItemList/ItemList';
+import { LoaderAnimation } from '../LoaderAnimation/LoaderAnimation';
+import { ResourceNotFound } from '../ResourceNotFound/ResourceNotFound';
 import { getFirestore } from '../../firebase/firebase';
 
 export const ItemListContainer = ({ categoryId='' }) => {
   const [items, setItems] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const selectView = (items) => {
+    let view = (
+      <ResourceNotFound> 
+        <p className='not-found-message'>We are sorry, category not found.</p>
+      </ResourceNotFound> 
+    );
+
+    if(items.length) {
+      view = (<ItemList products={items} />);
+    }
+
+    return view
+  }
 
   useEffect(() => {
+    setLoading(true);
     const db = getFirestore();
     const itemCollection = db.collection('products');
     let query;
@@ -21,8 +39,10 @@ export const ItemListContainer = ({ categoryId='' }) => {
     const unsuscribe = query.onSnapshot((querySnapshot) => {
       if (querySnapshot.empty) {
         console.log('No matching documents');
+      } else {
+        setItems(querySnapshot.docs.map((doc) => doc.data()));
       }
-      setItems(querySnapshot.docs.map((doc) => doc.data()));
+      setLoading(false);
     }, (error) => console.error(error))
 
     return () => unsuscribe();
@@ -30,7 +50,9 @@ export const ItemListContainer = ({ categoryId='' }) => {
   }, [categoryId]);
 
   return (
-    <ItemList products={items} />
+    <Fragment>
+      { loading ? <LoaderAnimation /> : selectView(items) }
+    </Fragment>
   );
 }
 
