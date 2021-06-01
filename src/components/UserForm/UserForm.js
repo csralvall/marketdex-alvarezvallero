b415aac89c11079ce56d0311e37191b99e8d0311
Cesar Alvarez Vallero <csralvarezvallero12@gmail.com>
2021-06-01 20:59:00 -0300

    Add confirmation to required fields
    It's usual to require users to reinsert some info in certain forms to
    assure that the data inserted is correct.
    
    Here, as the app doesn't have authentication, to mimic that behavior,
    the requested field will be the email. Although the logic has been
    designed to support any number of confirmation fields.

--- src/components/UserForm/UserForm.js
+++ src/components/UserForm/UserForm.js
@@ -1,13 +1,13 @@
 import './UserForm.css';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, Fragment } from 'react';
 
 import { Input } from '../Input/Input';
 
 export const UserForm = ({ onSubmit }) => {
   const [form, setForm] = useState({ name: '', phone: '', email: '', });
-
+  const [isConfirmed, setIsConfirmed] = useState(false);
+  const [isOrderSubmitted, setIsOrderSubmitted] = useState(false);
   const [isButtonDisabled, setIsButtonDisabled] = useState(true);
-
   const formFields = [
     {
       id:'name',
@@ -15,6 +15,7 @@ export const UserForm = ({ onSubmit }) => {
       value: form.name,
       validation: "^[a-zA-Z ,.'-]+$",
       required: true,
+      confirm: false,
     },
     {
       id:'phone',
@@ -22,6 +23,7 @@ export const UserForm = ({ onSubmit }) => {
       value: form.phone,
       validation: "^[0-9]{7,25}$",
       required: true,
+      confirm: false,
     },
     {
       id:'email',
@@ -29,6 +31,7 @@ export const UserForm = ({ onSubmit }) => {
       value: form.email,
       validation: "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:[.]{1}[a-zA-Z0-9-]{2,3})*$",
       required: true,
+      confirm: true,
     },
   ];
 
@@ -37,40 +40,71 @@ export const UserForm = ({ onSubmit }) => {
   const submitData = (e) => {
     const resetState = () => setForm({ name: '', phone: '', email: '' });
     onSubmit(e, form, resetState);
+    setIsOrderSubmitted(true);
   };
 
   const handleForm = (id, value) => {
     const newForm = { ...form, [id]: value };
     setForm(newForm);
   };
+
+  const handleConfirmation = (id, value) => {
+    (form[id] !== value) ? setIsConfirmed(false) : setIsConfirmed(true);
+  }
+
+  const setInput = ({id, label, value, validation, required, onChange}) => (
+    <Input
+      key={id}
+      id={id}
+      label={label}
+      value={value}
+      validation={validation}
+      required={required}
+      onChange={onChange} />
+  );
+
+  const setConfirmInput = (prefix, {id, label, validation, required, onChange}) => {
+    const newLabel = (
+      `${prefix[0].toUpperCase()}${prefix.substring(1)} ${label}`
+    );
+    return (
+      <Input
+        key={prefix+id}
+        id={id}
+        label={newLabel}
+        validation={validation}
+        required={required}
+        onChange={onChange} />
+    );
+  };
   
   useEffect(() => {
     const isSomeRequiredFieldEmpty = requiredFields.some(({ value }) => !value )
-    setIsButtonDisabled(isSomeRequiredFieldEmpty);
-  }, [requiredFields]);
+    setIsButtonDisabled(
+      isSomeRequiredFieldEmpty || !isConfirmed || isOrderSubmitted
+    );
+  }, [requiredFields, isConfirmed, isOrderSubmitted]);
 
   return (
     <form className='user-form' onSubmit={(e) => submitData(e)}>
       <div className='fields'>
-        {formFields.map(({ id, label, value, validation, required }) => (
-          <Input
-            key={id}
-            id={id}
-            label={label}
-            value={value}
-						validation={validation}
-						required={required}
-            onChange={handleForm} />
-        ))}
+        {formFields.map(({confirm, ...input }) => {
+          const fields = [setInput({onChange: handleForm, ...input})]
+          if (confirm) {
+            const confirmInput = {...input , onChange: handleConfirmation};
+            fields.push(setConfirmInput('confirm', confirmInput));
+          }
+          return fields;
+        })}
       </div>
       <button disabled={isButtonDisabled} type='submit'>
-        Create Order
-      </ button>
+        { isOrderSubmitted ? 'Submitting order...' : 'Create Order' }
+      </button>
       { requiredFields.length ? 
         <footer className='required-anotation'>
           * required fields
         </footer>
-      : null }
+        : <Fragment></Fragment> }
     </form>
   );
 }
